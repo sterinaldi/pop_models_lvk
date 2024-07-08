7d382b909af473396613cfd6cc88a6f8e2f49245
sterinaldi <stefano.rinaldi@uni-heidelberg.de>
2024-07-08 09:07:44 +0200

    added dvdz

--- population_models/redshift.py
+++ population_models/redshift.py
@@ -1,10 +1,22 @@
 import numpy as np
 from numba import njit
+from figaro.cosmology import dVdz_approx_planck18 as approx
 
-zmax = 2.3 # LVK paper
+zmax   = 2.3 # LVK paper
+z_norm = np.linspace(0,zmax,1000)
+dz     = z_norm[1]-z_norm[0]
 
 @njit
 def powerlaw(z, k):
-    p = ((k+1.)*(1.+z)**k)/((1.+zmax)**(k+1.)-1.)
-    p[z > zmax] = 0.
+    p = (1+z)**k
     return p
+
+@njit
+def _unnorm_powerlaw_redshift(z, k):
+    reg_const = (1+zmax)/approx(zmax)
+    return powerlaw(z, k)*approx(z)/(1+z) * reg_const
+
+@njit
+def powerlaw_redshift(z, k):
+    norm = np.sum(_unnorm_powerlaw_redshift(z_norm,k)*dz)
+    return _unnorm_powerlaw_redshift(z, k)/norm
