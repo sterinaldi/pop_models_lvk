0f79bd7c98e5cf552fe60c25067efa7c086c4de8
sterinaldi <stefano.rinaldi@uni-heidelberg.de>
2026-03-20 11:08:15 +0100

    all distributions plus bug fix

--- population_models/spin.py
+++ population_models/spin.py
@@ -1,5 +1,9 @@
 import numpy as np
 from math import erf
+from numba import njit
+
+nchi = np.linspace(-1,1,1000)
+dnchi = nchi[1]-nchi[0]
 
 @njit
 def truncated_gaussian(m, mu, sigma, mmin, mmax):
@@ -25,11 +29,17 @@ def joint_spin_tilt_angles(cost, mu, sigma, zeta):
 
 @njit
 def _skewed_gaussian_chieff_unnorm(chieff, mu, sigma, epsilon):
-    p_up   = peak(chieff, mu, sigma*(1-epsilon), -1., 1.)
-    p_down = peak(chieff, mu, sigma*(1+epsilon), -1., 1.)
-    return np.where(chieff < 0, p_down*(1+epsilon), p_up*(1-epsilon))
+    joint_up   = truncated_gaussian(np.zeros(1), mu, sigma*(1-epsilon), -1., 1.)*(1-epsilon)
+    joint_down = truncated_gaussian(np.zeros(1), mu, sigma*(1+epsilon), -1., 1.)*(1+epsilon)
+    p_up       = truncated_gaussian(chieff, mu, sigma*(1-epsilon), -1., 1.)*(1-epsilon)/joint_up
+    p_down     = truncated_gaussian(chieff, mu, sigma*(1+epsilon), -1., 1.)*(1+epsilon)/joint_down
+    return np.where(chieff < 0, p_down, p_up)
 
 @njit
 def skewed_gaussian_chieff(chieff, mu, sigma, epsilon):
     N = _skewed_gaussian_chieff_unnorm(nchi, mu, sigma, epsilon).sum()*dnchi
     return _skewed_gaussian_chieff_unnorm(chieff, mu, sigma, epsilon)/N
+
+@njit
+def gaussian_chip(chip, mu, sigma):
+    return truncated_gaussian(chip, mu, sigma, -1., 1.)
